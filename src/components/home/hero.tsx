ea2994974b7f386d1a7423fe57e33a21878e8def
prad-human-007 <kanasepradhumn@gmail.com>
2025-04-13 02:57:21 +0530

    made the color blue

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -3,8 +3,8 @@
 export function Hero() {
     return (
     <div className="flex flex-col items-center justify-center overflow-y-auto p-2 py-4 gap-4 w-full h-screen">
-      <h1 className="text-4xl text-violet-500"> Survey Monk</h1>
-      <a href="/voice" className="border border-gray-400 px-4 py-2 rounded-lg bg-white shadow-xl hover:bg-gray-50 transition-all duration-300 ease-in-out">
+      <h1 className="text-4xl text-blue-500"> Survey Monk</h1>
+      <a href="/voice" className="border border-gray-500 px-4 py-2 rounded-lg bg-white shadow-xl hover:bg-gray-50 transition-all duration-300 ease-in-out">
         Go to Demo page
       </a>
     </div>
